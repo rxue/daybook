0187b5190c0fb93b474d1c5ecccc71772c6464eb
Rui Xue <ruixue.fi@gmail.com>
2026-02-20 22:51:53 +0200

    till component

---
+++ programming/javascript/react/tutorial-demo/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  function ChatInput() {
+    return (
+      <div>
+        <input />
+        <button>Send</button>
+      </div>
+    );
+
+  }
+
+  const div = (
+    <div>
+      <ChatInput></ChatInput>
+    </div>)
+
+  return div
+}
+
+export default App
